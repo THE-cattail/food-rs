94c876d5bc56fa93655dd91165f2debe64062302
THE_cattail <THE_cattail@k-neco.moe>
2022-10-31 11:07:47 +0800

    fmt: beauty ask for continue

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
-use std::io;
+use std::io::{self, Write};
 
 /// # Errors
 ///
 /// Will return `Err` if `io::stdin().read_line` failed.
-pub fn ask_for_continue() -> Result<bool> {
+pub fn ask_for_continue(desc: &str) -> Result<bool> {
+    print!("Continue {desc}? (Y/n): ");
+    io::stdout().flush()?;
     let mut line = "".to_owned();
     io::stdin().read_line(&mut line)?;
     line = line.trim().to_owned();
