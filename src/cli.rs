f171935797806de5ce55ae1113558dfdc3fb555f
THE_cattail <THE_cattail@k-neco.moe>
2022-11-09 15:45:18 +0800

    feat: remove config and error

--- src/cli.rs
+++ src/cli.rs
@@ -1,16 +1,21 @@
 use std::io::{self, Write};
 
-use color_eyre::eyre::Result;
+use color_eyre::eyre;
+use eyre::{Result, WrapErr};
 
 /// # Errors
 ///
 /// Will return `Err` if `io::stdin().read_line` failed.
 pub fn ask_for_continue(desc: &str) -> Result<bool> {
     print!("{desc} (Y/n): ");
-    io::stdout().flush()?;
+    io::stdout()
+        .flush()
+        .wrap_err("Unable to ask for continue")?;
 
     let mut line = String::new();
-    io::stdin().read_line(&mut line)?;
+    io::stdin()
+        .read_line(&mut line)
+        .wrap_err("Unable to ask for continue")?;
 
     Ok(line.trim() != "n")
 }
--- src/error.rs
+++
@@ -1,6 +0,0 @@
-#[macro_export]
-macro_rules! result {
-    ($f:expr, $($args:tt)*) => {
-        ($f).map_err(|err| eyre::eyre!($($args)* err))
-    };
-}
